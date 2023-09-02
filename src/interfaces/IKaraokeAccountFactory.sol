0561be521ca91181e6c8fac80f3a7dae108f192f
byeongsu-hong <hong@byeongsu.dev>
2023-09-02 03:46:31 +0000

    base t

--- src/interfaces/IKaraokeAccountFactory.sol
+++ src/interfaces/IKaraokeAccountFactory.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface IKaraokeAccountFactory {}
+import "../account/KaraokeAccount.sol";
+
+interface IKaraokeAccountFactory {
+    function createAccount(address owner, uint256 salt) external returns (KaraokeAccount ret);
+
+    function getAddress(address owner, uint256 salt) external view returns (address);
+}
--- test/KaraokeAccount.t.sol
+++ test/KaraokeAccount.t.sol
@@ -3,6 +3,29 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
+import "../src/account/KaraokeAccount.sol";
+import "../src/account/KaraokeAccountFactory.sol";
+import "../src/account/ThresholdStore.sol";
+
 contract TestKaraokeAccount is Test {
-    function setUp() public {}
+    address owner;
+    address user;
+
+    ThresholdStore thresholdStore;
+    KaraokeAccount stdAcc;
+    KaraokeAccountFactory factory;
+
+    function setUp() public {
+        owner = address(0x1);
+        user = address(0x2);
+
+        thresholdStore = new ThresholdStore();
+        factory = new KaraokeAccountFactory(IEntryPoint(owner), thresholdStore);
+        stdAcc = factory.createAccount(user, 0);
+    }
+
+    function test_acc() public {
+        console.log("acc", address(stdAcc));
+        console.log("owner", stdAcc.owner());
+    }
 }
